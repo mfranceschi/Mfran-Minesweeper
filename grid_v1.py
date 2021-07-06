6858f46e75460715893c01795bd614246663a17d
mfranceschi <mfranceschi@omny.cloud>
2021-07-07 00:56:56 +0200

    update grid for cooler code

--- grid_v1.py
+++ grid_v1.py
@@ -8,4 +8,4 @@ Created on Tue Jul  6 23:43:41 2021
 class Grid_V1:
     def __init__(self):
         self._grid = []
-        self.magic_number = 42
+        self.game_is_running = False
--- grid_v1_test.py
+++ grid_v1_test.py
@@ -8,5 +8,5 @@ Created on Tue Jul  6 23:45:05 2021
 import pytest
 import grid_v1
 
-def test_magic_number():
-    assert grid_v1.Grid_V1().magic_number == 42
+def test_game_is_running():
+    assert grid_v1.Grid_V1().game_is_running == False
