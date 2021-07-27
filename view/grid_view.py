0874d01ab97553769010e645d6aacda70a14d3b0
mfranceschi <mfranc.martin@gmail.com>
2021-07-28 00:26:30 +0200

    refactor in view: Cell into its independent CellView class

--- view/grid_view.py
+++ view/grid_view.py
@@ -27,18 +27,19 @@ class GridView(tk.Frame):
             self.columnconfigure(column_index, minsize=25, weight=1)
 
         self.grid_to_display: List[str] = ["0"] * (size_x * size_y)
-        self.buttons: List[self.Cell] = [None] * len(self.grid_to_display)
+        self.buttons: List[CellView] = [None] * len(self.grid_to_display)
 
         for row_index in range(self.size_y):
             for column_index in range(self.size_x):
                 coord = Point2D(x=column_index, y=row_index)
 
-                button = self.Cell(
+                button = CellView(
                     cell_coord=coord,
                     master=self,
                     on_left_click=self.on_left_click,
                     on_right_click=self.on_right_click)
-                button.grid(row=row_index, column=column_index, sticky="nsew")
+                button.widget.grid(
+                    row=row_index, column=column_index, sticky="nsew")
                 self.buttons[self._index_of_point(coord)] = button
 
     def _index_of_point(self, point: Point2D) -> int:
@@ -48,53 +49,54 @@ class GridView(tk.Frame):
         for i, value in enumerate(grid):
             self.buttons[i].set_cell_value(value)
 
-    class Cell(tk.Button):
-        """
-        Wrapper class: configures a cell button.
-        When the cell updates please set the "cell_value" string.
-        """
 
-        def __init__(
-                self,
-                cell_coord: Point2D,
-                master: tk.Widget,
-                on_left_click: Callable[[Point2D], None],
-                on_right_click: Callable[[Point2D], None]
-        ):
-            super().__init__(master=master)
-            self.cell_coord = cell_coord
-            self.on_left_click = on_left_click
-            self.on_right_click = on_right_click
-            self.set_cell_value(" ")
+class CellView:
+    """
+    Wrapper class: configures a cell button.
+    When the cell updates please set the "cell_value" string.
+    """
 
-            self.bind("<ButtonRelease>", self.handle_button_event)
+    def __init__(
+            self,
+            cell_coord: Point2D,
+            master: tk.Widget,
+            on_left_click: Callable[[Point2D], None],
+            on_right_click: Callable[[Point2D], None]
+    ):
+        self.widget = tk.Button(master=master)
+        self.cell_coord = cell_coord
+        self.on_left_click = on_left_click
+        self.on_right_click = on_right_click
+        self.set_cell_value(" ")
 
-        def handle_button_event(self, event: tk.Event):
-            if event.num == 1:
-                # Left click
-                self.on_left_click(self.cell_coord)
-            elif event.num == 2 or event.num == 3:
-                # Middle or right click
-                self.on_right_click(self.cell_coord)
+        self.widget.bind("<ButtonRelease>", self.handle_button_event)
 
-        def set_cell_value(self, cell_value: str) -> None:
-            if cell_value == "0":
-                # Revealed, no neighbour
-                self.configure(bg="grey", text=" ", state="disabled")
+    def handle_button_event(self, event: tk.Event):
+        if event.num == 1:
+            # Left click
+            self.on_left_click(self.cell_coord)
+        elif event.num == 2 or event.num == 3:
+            # Middle or right click
+            self.on_right_click(self.cell_coord)
 
-            elif cell_value == "F":
-                # Not revealed, flag
-                self.configure(bg="yellow", text=" ", state="normal")
+    def set_cell_value(self, cell_value: str) -> None:
+        if cell_value == "0":
+            # Revealed, no neighbour
+            self.widget.configure(bg="grey", text=" ", state="disabled")
 
-            elif cell_value == "M":
-                # Revealed, mine
-                self.configure(bg="red", text=" ", state="disabled")
+        elif cell_value == "F":
+            # Not revealed, flag
+            self.widget.configure(bg="yellow", text=" ", state="normal")
 
-            elif cell_value == " ":
-                # Not revealed, no flag
-                self.configure(bg="blue", text=" ", state="normal")
+        elif cell_value == "M":
+            # Revealed, mine
+            self.widget.configure(bg="red", text=" ", state="disabled")
 
-            else:
-                # Revealed, has neighbours
-                self.configure(
-                    bg='white', text=cell_value, state="disabled")
+        elif cell_value == " ":
+            # Not revealed, no flag
+            self.widget.configure(bg="blue", text=" ", state="normal")
+
+        else:
+            # Revealed, has neighbours
+            self.widget.configure(
+                bg='white', text=cell_value, state="disabled")
